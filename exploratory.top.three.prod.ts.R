ee3e13de0621139ff7e957d2f32aecf5379ce572
Sam Pritchard <hamnsannah@gmail.com>
2019-01-10 01:35:33 -0500

    Add line graphs for top 3 products by supplier

--- exploratory.top.three.prod.ts.R
+++ exploratory.top.three.prod.ts.R
@@ -1,6 +1,7 @@
 exploratory.top.three.prod.ts <- function(supplier.filter, df.to.use){
   
   require(dplyr)
+  require(fpp2)
   exploratory.jh.time.series <- function(clean.df, freq = 365){
     require(lubridate)
     require(dplyr)
@@ -27,7 +28,21 @@ exploratory.top.three.prod.ts <- function(supplier.filter, df.to.use){
   }
   
   df.one.supplier <- filter(df.to.use, Supplier == supplier.filter)
+  agg.by.prod <- aggregate(Total.Sales ~ Description, df.one.supplier, sum)
+  agg.by.prod <- arrange(agg.by.prod, desc(Total.Sales))
+  top.prods <- agg.by.prod[c(1,2,3), 1]
   
+  prod1 <- filter(df.one.supplier, Description == top.prods[1])
+  prod1.ts <- exploratory.jh.time.series(prod1, freq = 12)
+  print(head(prod1.ts))
   
+  prod2 <- filter(df.one.supplier, Description == top.prods[2])
+  prod2.ts <- exploratory.jh.time.series(prod2, freq = 12)
   
+  prod3 <- filter(df.one.supplier, Description == top.prods[3])
+  prod3.ts <- exploratory.jh.time.series(prod3, freq = 12)
+  
+  joint.ts <- ts.union(prod1.ts, prod2.ts, prod3.ts)
+  colnames(joint.ts) <- top.prods
+  autoplot(joint.ts)
 }
